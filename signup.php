50d94d934d3520f068b7b418fb6488039664eebb
Evergreen Web Design <info@evergreenwebdesign.com>
2020-04-19 10:36:07 -0600

    added php-error.log to gitignore

--- signup.php
+++ signup.php
@@ -1,15 +1,11 @@
 <?php $layout_context = "signup"; ?>
-<?php
-// ini_set("display_errors","on");
-// error_reporting(E_ALL && ~E_NOTICE);
-?>
+
 <?php require_once 'controllers/authController.php'; ?>
 <?php include '_includes/head.php'; ?>
 
 <body>
 <?php require '_includes/nav.php'; ?>
-<?php // require '_includes/msg-one.php'; ?>
-<?php // require '_includes/msg-two.php'; ?>	
+	
 <img class="background-image" src="_images/aa-logo-dark_mobile.gif" alt="AA Logo">
 <div id="landing">
 	<form action="" method="post">
@@ -28,7 +24,7 @@
         <input type="password" class="text" name="password" placeholder="Password">
         <input type="password" class="text" name="passwordConf" placeholder="Confirm password">
         <!-- <button type="submit" name="signup-btn">Sign Up</button> -->
-		<input type="submit" name="submit" class="submit" value="Submit">
+		<input type="submit" name="submit" class="submit" value="Sign up">
         <p class="btm-p">Already a member? <a class="log" href="login.php">Sign in</a></p>
 	</form>
     
