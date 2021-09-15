29cef4436b2f7bb8eb4a14bdfcd94a61957fe09c
Evergreen Web Design <robert@robertmeans.com>
2021-09-15 14:09:45 -0600

    Update msg-why-join.php

--- _includes/msg-why-join.php
+++ _includes/msg-why-join.php
@@ -6,7 +6,7 @@
 
 			<p>This site offers a way to share, find and organize meeting information.</p>
 
-			<p>Opening an account allows you post on this site. You can publish meeting information and choose to have that available to everyone, to only other members of EvergreenAA.com or keep it private - for your eyes only. You can manage the information you put on this website (create, edit, delete - via mobile, tablet or desktop) and you can also participate in the Message Board.</p>
+			<p>Opening an account allows you post on this site. You can publish meeting information and make it available to everyone who visits this site, to only other members of EvergreenAA.com or keep it private - for your eyes only. You can manage the information you put here (create, edit, delete) via mobile, tablet or desktop) and you can also participate on the Message Board. <span style="font-size: 0.8em;color:rgba(255,255,255,0.6);">(...It's Wednesday, September 15, 2021; No seriously, will someone please participate on the Message Board?!)</span></p>
 
 		</div><!-- .inside-msg-one -->
 	</div><!-- .msg-bkg -->
