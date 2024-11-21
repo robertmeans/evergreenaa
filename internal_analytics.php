4099f109c3962c9a64c4c7dba11a0f60cd0b05d6
Robert Means <robert@robertmeans.com>
2024-11-20 18:50:44 -0700

    clicking along

--- internal_analytics.php
+++ internal_analytics.php
@@ -32,36 +32,32 @@ require '_includes/head.php'; ?>
 <?php $theme = configure_theme(); mobile_bkg_config($theme); ?>
 <?php
   $results = get_analytic_data();
-  /* reference:   id  occurred  auser_email page  day_opened  mtg_opened  a_ip */
+  /* fields are:   id,  occurred,  auser_email, page,  day_opened,  mtg_opened,  a_ip */
   $i = 0;
   $ip_groups = [];
-
   $unique_count = 0;
   $unique_ips = [];
 
   while ($row = mysqli_fetch_assoc($results)) {
-    $array = $row;
 
-    $ip = $row['a_ip'];
+    $all_records[] = $row; /* grab everything for later processing */
+
+    $ip = $row['a_ip']; /* 'bout to do tricky stuff based on IP addresses... */
     /* get visits that are likely bots due to no interactions, just page visits */
     if (!isset($ip_groups[$ip])) {
         $ip_groups[$ip] = [$row];
     } else {
         $ip_groups[$ip][] = $row;
     }
-
     /* consolidate unique ip's for a count of how many people are using the site */
     if (!in_array($ip, $unique_ips)) {
         $unique_ips[] = $ip;
         $unique_count++;
     }
 
-    /* get total number of rows total interactions */
-    $i++;
+    $i++; /* get total number of rows - everything included */
   }
-
-
-  
+ 
 ?>
 <div id="manage-wrap">
   
@@ -71,17 +67,13 @@ require '_includes/head.php'; ?>
 
   <div class="manage-simple analy"> 
     <h1 class="my-meet">Internal Analytics</h1><p class="my-sort"><a class="phpma" href="<?php 
-    if (WWW_ROOT == 'http://localhost/evergreenaa') { ?>http://localhost/phpmyadmin/<?php 
+    if (WWW_ROOT === 'http://localhost/evergreenaa') { ?>http://localhost/phpmyadmin/<?php 
     } else { ?>https://p3plzcpnl504722.prod.phx3.secureserver.net:2083/cpsess0249341861/frontend/jupiter/sql/PhpMyAdmin.html<?php 
     } ?>" target="_blank">phpMyAdmin</a></p>
   </div>
 
-
-
-
-
-<?php  /* this grabs each unique ip - an ip that only appears once */
-
+<?php  
+/* begin - this grabs each unique ip */
 $single_visit_no_action = []; // $unique_row['a_ip'];
 $multiple_visits_no_action = []; // $multiple_but_same_ip;
 
@@ -112,13 +104,32 @@ foreach ($ip_groups as $multiple_but_same_ip => $rows) {
     }
   }
 }
+/* end - this grabs each unique ip */
+
+/* begin - get first row date to use as "analytics started" date */
+foreach ($all_records as $row) {
+  $analytics_start_date = $row['occurred'];
+  break;
+}
+/* end - get first row date */
+
+/* begin - homepage only count */
+  $homepage_loads = 0;
+  foreach ($all_records as $row) {
+    if (!empty($row['page']) && empty($row['day_opened']) && empty($row['mtg_opened'])) {
+      $homepage_loads++;
+    }
+  }
+/* end - homepage only count */
 ?>
-
-
   <div class="ia-headwrap">
     <?php // start grabbing data and filling in page ?>
     <p class="ail"><?php
-      echo $i . ' Interactions | ';
+
+      $dateTime = DateTime::createFromFormat('H:i:s D, m.d.y', $analytics_start_date);
+      $new_start_formatted = $dateTime->format('l, F d, Y \a\t H:i:s A');
+
+      echo 'Since: ' . $new_start_formatted . '<br>' . $i - $homepage_loads . ' Interactions | ';
 
       if ($unique_count == 1 ) { echo $unique_count . ' unique IP'; } 
       if ($unique_count == 0 || $unique_count > 1) { echo $unique_count . ' unique IP\'s'; } 
@@ -137,6 +148,7 @@ foreach ($ip_groups as $multiple_but_same_ip => $rows) {
 
 <?php if ($single_visit_no_action || $multiple_visits_no_action) { ?>
   <div id="replace-this" class="ia-ip-list">
+  <?php if ($single_visit_no_action) { ?>
     <div class="col unique-ips">
       <h1>Unique IP</h1>
       <p class="ip-notes">appears only once, no actions taken</p>
@@ -149,6 +161,7 @@ foreach ($ip_groups as $multiple_but_same_ip => $rows) {
 
       ?>
     </div>
+  <?php } if ($multiple_visits_no_action) { ?>
     <div class="col multi-visits">
       <h1>Multiple visits</h1>
       <p class="ip-notes">no action taken</p>
@@ -161,6 +174,7 @@ foreach ($ip_groups as $multiple_but_same_ip => $rows) {
 
       ?>
     </div>
+  <?php } ?>
   </div>
 <?php } ?>
 
