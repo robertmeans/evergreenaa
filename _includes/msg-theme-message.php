ab6068dbf57d69f7751cf955fa8f9f25bc66e476
Robert Means <robert@robertmeans.com>
2024-11-25 12:49:52 -0700

    still solid
    now counting cookies so the popup doesn't appear until $i = 1 (2nd pass)

--- _includes/msg-theme-message.php
+++ _includes/msg-theme-message.php
@@ -9,15 +9,15 @@
 			<i class="far fa-times-circle"></i>
 			<h1><i class="far fas fa-lightbulb"></i>&nbsp; Light this sucker up!</h1>
 
-			<p>Hey<?= $greeting ?>, did you know there's a "Bright Theme" option? Just in case the long days of winter leave you wanting to chipper up this joint - open the Menu and you'll find it at the top.</p>
+			<p>Hey<?= $greeting ?>, did you know there's a "Bright Theme" option? Just in case the hazy shade of winter leaves you longing for a lively lift - open the Menu and you'll find it at the top. Or...</p>
 
       <form action="process-theme.php" method="post">
         <input type="hidden" name="theme" value="1">
         <input type="hidden" id="themepopupurl" name="themeurl">
-        <a class="theme-popup-btn" onclick="$(this).closest('form').submit(); closeNav();">click here to try it now</a>
+        <a class="theme-popup-btn" onclick="$(this).closest('form').submit(); closeNav();">Click here to try it now</a>
       </form>
 
-      <?php /* <p class="poprt">This message should only appear once.</p> */ ?>
+      <?php /* <p class="poprt">I'm doin' my darndest to make sure you only see this message once.</p> */ ?>
 		</div><!-- .inside-msg-one -->
 	</div><!-- .msg-bkg -->
 </div><!-- #msg-two -->
--- process-theme-popup.php
+++ process-theme-popup.php
@@ -9,19 +9,22 @@ require_once 'config/initialize.php';
 */
 
 if (!isset($theme_popup_on_off)) { return; } else {
+  
+  $i = 0;
 
   // if (empty($_COOKIE['theme-popup'])) { /* dev */
   if (!empty($_COOKIE['theme-popup'])) { /* production */
-
     $popup_signal = 'nope';
-
+  } else if (!isset($_COOKIE['theme-popup-count']) || $_COOKIE['theme-popup-count'] < 1) {
+    $i++; /* show on 2nd visit just to make sure they've seen the dark theme first */
+    setCookie('theme-popup-count', $i, time() + (3650 * 24 * 60 * 60), '/'); // 10 years
+    $popup_signal = 'nope';
   } else {
     /* just a layer of reduncancy to ensure nobody accidently sees this popup a 2nd time */
     if (  (!empty($_COOKIE['sessionTheme']) && $_COOKIE['sessionTheme'] == '1') || 
           (isset($_SESSION['db-theme']) && $_SESSION['db-theme'] == '1')      ) {
       $popup_signal = 'nope';
     } else {
-
       setCookie('theme-popup', 'shown', time() + (3650 * 24 * 60 * 60), '/'); // 10 years
       $popup_signal = 'ok'; 
 
